84b8eea4b24a1cad3e3da99c15beb986fda36d24
Ximena <ximena.velasco@cceo.com.mx>
2024-03-18 10:58:53 -0600

    feat: :sparkles: Routes, db configuration, migrations, controllers artisan, models

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacto', function () {
+    return "Contactame";
+})->name('contacto');
+
+Route::get('/custom', function () {
+    
+    $msj2 = "Mensaje desde el servidor *-*";
+
+    //return view('custom', ['msj' => $msj2, "edad" => 15]);
+    
+    $data = ['msj' => $msj2, "edad" => 15];
+    
+    return view('custom', $data);
+});
