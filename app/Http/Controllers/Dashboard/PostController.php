5c2a9e4d6e3a6e4bec5a33b266df871a9659e46b
Ximena <ximena.velasco@cceo.com.mx>
2024-03-19 02:43:54 -0600

    feat(crud-post): :sparkles: Resource type and model controller, resource type vs traditional routes

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -3,24 +3,17 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
-class TestController extends Controller
-
-
+class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $posts = [1,2,3,4,'Andres'];
-        $name = "Andres";
-
-        /**
-         * ['posts' => $posts] === compact('posts)
-         */
-        return view("dashboard.test.index", compact('posts', 'name'));
+        //
     }
 
     /**
@@ -42,7 +35,7 @@ class TestController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
         //
     }
@@ -50,7 +43,7 @@ class TestController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
         //
     }
@@ -58,7 +51,7 @@ class TestController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
         //
     }
@@ -66,7 +59,7 @@ class TestController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 
 <?php
 
-use App\Http\Controllers\Dashboard\TestController;
+use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[TestController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::resource('post', PostController::class);
+
+Route::get('post', [PostController::class,'index']);
+Route::get('post/{post}', [PostController::class,'show']);
+Route::get('post/create', [PostController::class,'create']);
+Route::get('post/{post}', [PostController::class,'edit']);
+
+Route::post('post', [PostController::class,'store']);
+Route::put('post/{post}', [PostController::class,'update']);
+Route::delete('post/{post}', [PostController::class,'delete']);
