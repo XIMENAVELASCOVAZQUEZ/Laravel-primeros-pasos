542d8b57e31ead6b5d55f09832be4c39c8c484ae
Ximena <ximena.velasco@cceo.com.mx>
2024-03-21 03:01:45 -0600

    feat(Routes): :sparkles: Optional arguments, grouped, resource type

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    {{ $user->name}}
+    <h1>Bienvenido</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\PostController;
+use App\Http\Middleware\TestMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,8 +21,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-Route::resource('category', CategoryController::class);
+Route::middleware([TestMiddleware::class])->group(function() {
+    Route::get('/test/{id?}/{name?}',function($id = 10, $name="pepe") {
+    echo $id;
+    echo $name;
+    });
+});
+
+Route::group(['prefix' => 'dashboard'],function() {
+    //Route::resource('post', PostController::class);
+    //Route::resource('post', PostController::class)->only(['show']);
+    Route::resource('post', PostController::class)->except(['show']);
+    //Route::resource('category', CategoryController::class);
+
+    // Route::resources([
+    //     'post' => PostController::class,
+    //     'category' => CategoryController::class,
+    // ]);
+});
+
+// Route::get('/test/{id?}/{name?}',function($id = 10, $name="pepe") {
+//     echo $id;
+//     echo $name;
+// });
+
+// Route::controller(PostController::class)->group(function()
+// {
+//     Route::get('post','index')->name("post.index");
+//     Route::get('post/{post}','show')->name("post.show");
+//     Route::get('post/create','create')->name("post.create");
+//     Route::get('post/{post}','edit')->name("post.edit");
+
+//     Route::post('post','store')->name("post.store");
+//     Route::put('post/{post}','update')->name("post.update");
+//     Route::delete('post/{post}','delete')->name("post.destroy");
+// });
+
 
 // Route::get('post', [PostController::class,'index']);
 // Route::get('post/{post}', [PostController::class,'show']);
